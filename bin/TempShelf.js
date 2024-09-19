3efc01f9bd5387c886ddf71439ced6fdd5d1f900
unknown <kazumaproject7@gmail.com>
2024-09-19 09:31:31 -0400

    update package.json

--- bin/TempShelf.js
+++ bin/TempShelf.js
@@ -1,11 +1,5 @@
 #!/usr/bin/env node
 
 import { execSync } from "child_process";
-import { join, path } from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const projectDir = join(__dirname, "..");
-
-execSync("npm run start", { cwd: projectDir, stdio: "inherit" });
+execSync("npm run start");
