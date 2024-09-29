d85c340dcc387f99f7af1eb335d7e67a3ebd6248
unknown <kazumaproject7@gmail.com>
2024-09-29 01:00:14 -0400

    npm コマンドの修正
    temp-shelf がインストールされているかチェックする
    
    インストールされていなければインストールする

--- bin/temp-shelf.js
+++ bin/temp-shelf.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 
 import { execSync } from "child_process";
+import { existsSync } from "fs";
 
 try {
+  if (!existsSync("node_modules/temp-shelf")) {
+    console.log("temp-shelf is not installed. Installing...");
+    execSync("npm install temp-shelf", { stdio: "inherit" });
+    console.log("temp-shelf installed successfully.");
+  }
   execSync("cd node_modules/temp-shelf/ && npm start", { stdio: "inherit" });
   console.log("npm start executed successfully in temp-shelf");
 } catch (error) {
